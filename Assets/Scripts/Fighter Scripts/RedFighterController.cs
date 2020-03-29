6b6b1a30249d6bb11492fb61c6c0593f60f3e0b9
bradstar879 <bradstar879@yahoo.com>
2020-03-28 20:33:25 -0500

    Added missing files

---
+++ Assets/Scripts/Fighter Scripts/RedFighterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RedFighterController : FighterContoller
+{
+    public override void Init(GameObject otherFighter)
+    {
+        base.Init(otherFighter);
+        introQuotes = new string[]{
+            "Call me Freddie because I'm you're worst nightmare.",
+            "Bring it on dingus. I could eat your ass all day.",
+            "You're going down. I mean all the way down. Like downtown down."};
+        victoryQuotes = new string[]{
+            "Knew you couldn't handle this D.",
+            "What an amateur.",
+            "You're not even worth a rematch."};
+    }
+}
