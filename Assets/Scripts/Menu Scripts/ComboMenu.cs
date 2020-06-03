8a48e89b7cce9789709bf747e1b9e05634ea90f8
bradstar879 <bradstar879@yahoo.com>
2020-06-02 23:02:51 -0500

    Added missing created files

---
+++ Assets/Scripts/Menu Scripts/ComboMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComboMenu : BaseMenuScript
+{
+    [SerializeField] Button basicCombosButton;
+    [SerializeField] Button advancedCombosButton;
+    [SerializeField] Button backButton;
+
+    [SerializeField] GameObject basicCombos;
+    [SerializeField] GameObject advancedCombos;
+    [SerializeField] GameObject pauseMenu;
+
+    private GameManager gameManager;
+
+    protected override void Init()
+    {
+        buttonMap = new Button[,] { { basicCombosButton, advancedCombosButton, backButton } };
+        gameManager = GameObject.FindWithTag("Game Manager").GetComponent<GameManager>();
+    }
+
+    protected override void ResetMenu()
+    {
+        ShowBasicCombos();
+    }
+
+    private void OnEnable()
+    {
+        ResetMenu();
+        if (gameManager.IsPlayerOnePaused())
+        {
+            LoadMenu();
+        }
+        else
+        {
+            menuNavigation.LoadMenu(this, buttonMap, 0, 0, 0, 0);
+            menuNavigation.DeactivateMenu();
+        }
+    }
+
+    public void ShowBasicCombos()
+    {
+        basicCombos.SetActive(true);
+        advancedCombos.SetActive(false);
+    }
+
+    public void ShowAdvancedCombos()
+    {
+        basicCombos.SetActive(false);
+        advancedCombos.SetActive(true);
+    }
+
+    public void Back()
+    {
+        pauseMenu.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Menu Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : BaseMenuScript
+{
+    [SerializeField] Button resumeButton;
+    [SerializeField] Button combosButton;
+    [SerializeField] Button mainMenuButton;
+
+    [SerializeField] GameObject comboMenu;
+    private GameManager gameManager;
+
+    protected override void Init()
+    {
+        buttonMap = new Button[,] { { resumeButton, combosButton, mainMenuButton } };
+        gameManager = GameObject.FindWithTag("Game Manager").GetComponent<GameManager>();
+    }
+
+    private void OnEnable()
+    {
+        ResetMenu();
+        if (gameManager.IsPlayerOnePaused())
+        {
+            LoadMenu();
+        }
+        else
+        {
+            menuNavigation.LoadMenu(this, buttonMap, 0, 0, 0, 0);
+            menuNavigation.DeactivateMenu();
+        }
+    }
+
+    public void DisplayCombos()
+    {
+        comboMenu.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+}
