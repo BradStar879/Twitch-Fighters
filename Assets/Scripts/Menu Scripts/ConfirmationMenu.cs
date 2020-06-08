b40fc2dc6c81ec1f14efaeff7181915c1f2c5324
bradstar879 <bradstar879@yahoo.com>
2020-06-08 02:27:29 -0500

    Added menu scripts

---
+++ Assets/Scripts/Menu Scripts/ConfirmationMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ConfirmationMenu : BaseMenuScript
+{
+    [SerializeField] Button yesButton;
+    [SerializeField] Button noButton;
+
+    protected override void Init()
+    {
+        buttonMap = new Button[,] { { yesButton, noButton } };
+    }
+
+    public void LoadFightScene()
+    {
+        SceneManager.LoadScene("Fight Scene");
+    }
+}
---
+++ Assets/Scripts/Menu Scripts/DifficultyMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyMenu : BaseMenuScript
+{
+    [SerializeField] Button easyButton;
+    [SerializeField] Button mediumButton;
+    [SerializeField] Button hardButton;
+    [SerializeField] GameObject confirmationMessage;
+    private GameManager gameManager;
+
+    protected override void Init()
+    {
+        buttonMap = new Button[,] { { easyButton },
+                                    { mediumButton },
+                                    { hardButton } };
+        gameManager = GameObject.FindWithTag("Game Manager").GetComponent<GameManager>();
+    }
+
+    public void EasyCpu()
+    {
+        GameData.SetComputerDifficulty(Difficulty.Easy);
+        confirmationMessage.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void MediumCpu()
+    {
+        GameData.SetComputerDifficulty(Difficulty.Medium);
+        confirmationMessage.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void HardCpu()
+    {
+        GameData.SetComputerDifficulty(Difficulty.Hard);
+        confirmationMessage.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
